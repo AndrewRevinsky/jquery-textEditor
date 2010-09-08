0fe6895e67fc8c5af44b9b7ab504b5b393e64267
Colin Drake <colin.f.drake@gmail.com>
2010-09-07 20:17:20 -0400

    Adds new unified text library and does text selection upon insertion

--- jquery.formattedTextarea.js
+++ jquery.formattedTextarea.js
@@ -38,14 +38,17 @@
             .addClass(options.li_class)
             .click(function() {
               // Get selection and replacement
-              var selection = $(obj).getSelection().text;
+              var selection = $(obj).getSelection();
               var replacement = options.controls[$(this).html()];
 
               // Either insert or replace text
-              if (selection == '') {
-                $(obj).insertAtCaret(replacement.replace(options.substitution_string, options.prompt_string));
+              if (selection.text == '') {
+                var r = new String(replacement.replace(options.substitution_string, options.prompt_string));
+                var index = r.indexOf(options.prompt_string);
+                $(obj).insertAtCaretPos(r);
+                $(obj).setSelection(selection.start + index, selection.start + index + options.prompt_string.length);
               } else {
-                $(obj).replaceSelection(replacement.replace(options.substitution_string, selection), true);
+                $(obj).replaceSelection(replacement.replace(options.substitution_string, selection.text), true);
               }
 
               return false;
@@ -56,123 +59,3 @@
     }
   });
 })(jQuery);
-/*
- * Taken from: http://www.mail-archive.com/jquery-en@googlegroups.com/msg08708.html
- * Insert content at caret position (converted to jquery function)
- * @link http://alexking.org/blog/2003/06/02/inserting-at-the-cursor-using-javascript
- */
-$.fn.insertAtCaret = function (myValue)
-{
-  return this.each(function ()
-  {
-    //IE support
-    if (document.selection)
-    {
-      this.focus();
-      sel = document.selection.createRange();
-      sel.text = myValue;
-      this.focus();
-    }
-    //MOZILLA/NETSCAPE support
-    else if (this.selectionStart || this.selectionStart == '0')
-    {
-      var startPos = this.selectionStart;
-      var endPos = this.selectionEnd;
-      var scrollTop = this.scrollTop;
-      this.value = this.value.substring(0, startPos) + myValue + this.value.substring(endPos, this.value.length);
-      this.focus();
-      this.selectionStart = startPos + myValue.length;
-      this.selectionEnd = startPos + myValue.length;
-      this.scrollTop = scrollTop;
-    }
-    else
-    {
-      this.value += myValue;
-      this.focus();
-    }
-  });
-};
-/*
- * jQuery plugin: fieldSelection - v0.1.0 - last change: 2006-12-16
- * (c) 2006 Alex Brem <alex@0xab.cd> - http://blog.0xab.cd
- */
-(function ()
-{
-  var fieldSelection =
-  {
-    getSelection: function ()
-    {
-      var e = this.jquery ? this[0] : this;
-      return ( /* mozilla / dom 3.0 */ ('selectionStart' in e &&
-      function ()
-      {
-        var l = e.selectionEnd - e.selectionStart;
-        return {
-          start: e.selectionStart,
-          end: e.selectionEnd,
-          length: l,
-          text: e.value.substr(e.selectionStart, l)
-        };
-      }) || /* exploder */ (document.selection &&
-      function ()
-      {
-        e.focus();
-        var r = document.selection.createRange();
-        if (r == null)
-        {
-          return {
-            start: 0,
-            end: e.value.length,
-            length: 0
-          }
-        }
-        var re = e.createTextRange();
-        var rc = re.duplicate();
-        re.moveToBookmark(r.getBookmark());
-        rc.setEndPoint('EndToStart', re);
-        return {
-          start: rc.text.length,
-          end: rc.text.length + r.text.length,
-          length: r.text.length,
-          text: r.text
-        };
-      }) || /* browser not supported */
-
-      function ()
-      {
-        return {
-          start: 0,
-          end: e.value.length,
-          length: 0
-        };
-      })();
-    },
-    replaceSelection: function ()
-    {
-      var e = this.jquery ? this[0] : this;
-      var text = arguments[0] || '';
-      return ( /* mozilla / dom 3.0 */ ('selectionStart' in e &&
-      function ()
-      {
-        e.value = e.value.substr(0, e.selectionStart) + text + e.value.substr(e.selectionEnd, e.value.length);
-        return this;
-      }) || /* exploder */ (document.selection &&
-      function ()
-      {
-        e.focus();
-        document.selection.createRange().text = text;
-        return this;
-      }) || /* browser not supported */
-
-      function ()
-      {
-        e.value += text;
-        return this;
-      })();
-    }
-  };
-  jQuery.each(fieldSelection, function (i)
-  {
-    jQuery.fn[i] = this;
-  });
-})();
